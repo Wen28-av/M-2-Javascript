184714d7fdb0db15d81dc4c8f839ee0671403196
Wen28-av <126975369+Wen28-av@users.noreply.github.com>
2024-01-15 21:42:38 -0400

    M-2 Jvascript.

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+/*Ejemplo uno de comentario / comentario en bloque=> apretando alt+shift+A*/
+//Ejemplo de comentario =>comentario en linea=> utilizando doble /=> CTRL+}
+/*Console.log =>para mostrar en consola lo expresado entre los parentesis*/
+console.log("Esta es una suma=>"2+2)
+/*document.write => para mostrar en antalla lo expresado entre*/
+document.write("Bienvenidos a DEV.F")

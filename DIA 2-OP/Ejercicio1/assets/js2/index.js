99c3fe57c7385f5df2a2be71f9944c921453c0a5
Wen28-av <126975369+Wen28-av@users.noreply.github.com>
2024-01-30 17:36:19 -0400

    dia 2

---
+++ DIA 2-OP/Ejercicio1/assets/js2/index.js
@@ -0,0 +1,42 @@
+// en estas primeras 6 preguntas, reemplaza "null" por la respuesta
+/* Crea una variable "string" puede contener lo que quieras:*/
+const nuevastring = "Hola mundo de oportunidades";
+
+/* Crea una variable numerica puede ser cualquier numero: */
+const nuevoNum = 24;
+
+/* Crea una variable booleana */
+const nuevoBool = false;
+
+/* Resuelve el siguiente problema matematico: */
+const nuevaResta = 188-45;
+
+/* Resuelve el siguiente problema matematico: */
+const nuevaMultiplicacion = 52*5;
+
+/* Resuelve el siguiente problema matemático reemplazando null para que el resultado final sea 1: */
+const nuevoModulo = 21 %  7 === 1;
+
+ console.log("=>" ,21 % 4);
+
+/* ----------------Ejercicio 1------------------------------- */
+// Ejercicio 1: Crear variables de diferentes tipos de datos y luego consologuear su valor.
+// a. Crear una variable llamada "nombre" y asignarle tu nombre como cadena/string.
+let nombre = "Wendy";
+// b. Crear una variable llamada "edad" y asignarle tu edad como número.
+
+// c. Crear una variable llamada "esEstudiante" y asignarle un valor booleano que indique si eres estudiante o no.
+
+// d. Crear una variable llamada "nulo" y asignarle el valor nulo/null.
+
+/* ----------------------------------------------------------------------------------- */
+// Ejercicio 3: Operaciones matemáticas
+// a. Crear dos variables, "numero1" y "numero2", y asignarles valores numéricos.
+// b. Calcular la suma, resta, multiplicación y división de los dos números (en nuevas variables) y mostrar los resultados en la consola.
+ 
+
+//1
+//2
+let suma = numer1 + numer2
+console.log("Suma=>",suma)
+

9f6e7bd6ef64b33010f23f005f15df83e14b9951
Wen28-av <126975369+Wen28-av@users.noreply.github.com>
2024-02-04 15:16:46 -0400

    nuevo operadores

--- DIA 2-OP/Ejercicio1/assets/js2/index.js
+++ DIA 2-OP/Ejercicio1/assets/js2/index.js
@@ -5,7 +5,7 @@ const nuevastring = "Hola mundo de oportunidades";
 /* Crea una variable numerica puede ser cualquier numero: */
 const nuevoNum = 24;
 
-/* Crea una variable booleana */
+/* Crea una variable booleana  true or false*/
 const nuevoBool = false;
 
 /* Resuelve el siguiente problema matematico: */
@@ -15,20 +15,22 @@ const nuevaResta = 188-45;
 const nuevaMultiplicacion = 52*5;
 
 /* Resuelve el siguiente problema matemático reemplazando null para que el resultado final sea 1: */
-const nuevoModulo = 21 %  7 === 1;
+const nuevoModulo = 21 %  5 === 1;
 
- console.log("=>" ,21 % 4);
+ console.log("=>" ,21 % 5);
 
 /* ----------------Ejercicio 1------------------------------- */
 // Ejercicio 1: Crear variables de diferentes tipos de datos y luego consologuear su valor.
 // a. Crear una variable llamada "nombre" y asignarle tu nombre como cadena/string.
 let nombre = "Wendy";
 // b. Crear una variable llamada "edad" y asignarle tu edad como número.
-
+let edad = 25;
 // c. Crear una variable llamada "esEstudiante" y asignarle un valor booleano que indique si eres estudiante o no.
-
+let esEstudiante = true;
 // d. Crear una variable llamada "nulo" y asignarle el valor nulo/null.
+let nulo = null;
 
+console.log(nombre, edad, esEstudiante, nulo);
 /* ----------------------------------------------------------------------------------- */
 // Ejercicio 3: Operaciones matemáticas
 // a. Crear dos variables, "numero1" y "numero2", y asignarles valores numéricos.
@@ -37,6 +39,16 @@ let nombre = "Wendy";
 
 //1
 //2
-let suma = numer1 + numer2
-console.log("Suma=>",suma)
+let numero1 = 12
+let numero2 = 21
 
+let suma = numero1 + numero2;
+let resta = numero1 - numero2;
+let multiplicación = numero1 * numero2;
+let division = numero1 / numero2;
+
+console.log("Resultados del ejercicio 3 =======>");
+console.log("Suma=>", suma);
+console.log("Resta=>", resta);
+console.log("multiplicacion=>", multiplicación);
+console.log("Division=>", division);
---
+++ DIA 2-OP/Operadores/assets/js/index.js
@@ -0,0 +1,20 @@
+console.log("archivo conectado")
+
+/* ---------------------OPERADORES MATEMATICOS -------------*/
+/* las operaciones matematicas arrojan un valor de salidas unico */
+let numeroFavorito = 44;
+let suma = numeroFavorito+2 //46
+let resta = 5-3 //2
+let multiplicacion = 5*5; //25
+let division = 25/5; //5
+let residuoDeDivision = 25% 5 // 0
+let residuoDeDivision2 = 28% 5 // 25/5=>0 para llegar al 28 falta 3 por eso el residuo es>>> 3=====>>> Sirve para saber si el numero es par
+let esPar = 29%2
+let potencia = 2 ** 3; //8
+/*console.log(esPar);*/
+/* ----------OPERADORES RELACIONALES -------------------*/
+
+let esCorrecto = 5 + 2 === 4 + 3;
+//                 7   ===   7 ===> true
+console.log(esCorrecto);
+//mimuto  1:29
